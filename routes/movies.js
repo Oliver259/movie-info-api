eb69e16ba9166df3c4bc6eee7117abc7b8f54c0f
Oliver259 <115050339+Oliver259@users.noreply.github.com>
2023-05-24 12:56:13 +1000

    Updated search to be usable by swagger docs

--- routes/movies.js
+++ routes/movies.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 const authorization = require("../middleware/authorization");
 
-router.get("/search/:primaryTitle", function (req, res) {
-  const searchTerm = req.params.primaryTitle;
+router.get("/search", function (req, res) {
+  const searchTerm = req.query.title;
   req.db
     .from("basics")
     .select(
