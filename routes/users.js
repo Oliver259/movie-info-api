5ddfa798c89c1063fc254634e77cd07236d6aa08
Oliver259 <115050339+Oliver259@users.noreply.github.com>
2023-05-25 17:13:59 +1000

    Added refresh tokens

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,31 @@ var router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
+// TODO: Add refresh tokens
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.json({ error: false });
 });
 
 router.post("/login", function (req, res, next) {
-  // 1. Retrieve email and password from req.body
+
   // Retrieve email and password from req.body
   const email = req.body.email;
   const password = req.body.password;
+  const bearerExpiresIn = req.body.bearerExpiresInSeconds;
+  const refreshExpiresIn = req.body.refreshExpiresInSeconds;
 
   // Verify body
-  if (!email || !password) {
+  if (!email || !password || !bearerExpiresIn || !refreshExpiresIn) {
     res.status(400).json({
       error: true,
       message: "Request body incomplete - email and password needed",
     });
     return;
   }
-  // 2. Determine if user already exists in table
+  // Determine if user already exists in table
   const queryUsers = req.db
     .from("users")
     .select("*")
@@ -33,26 +37,40 @@ router.post("/login", function (req, res, next) {
       if (users.length === 0) {
         throw new Error("User does not exist");
       }
-      // 2.1 If user does exist, verify if passwords match
+      // If user does exist, verify if passwords match
       const user = users[0];
       return bcrypt.compare(password, user.hash);
     })
-    // 2.1.2 If passwords do not match, return error response
+    // If passwords do not match, return error response
     .then((match) => {
       if (!match) {
-        throw new Error("Passwords do not match");
+        throw new Error("Incorrect email or password");
       }
-      // Create and return JWT token
-      const expires_in = 60 * 60 * 24; // 24 hours
-      const exp = Math.floor(Date.now() / 1000 + expires_in);
-      const token = jwt.sign({ email, exp }, process.env.JWT_SECRET);
+      // Create bearer and refresh tokens
+      const bearerExp = Math.floor(Date.now() / 1000 + bearerExpiresIn); 
+      const refreshExp = Math.floor(Date.now() / 1000 + refreshExpiresIn);
+      const bearerToken = jwt.sign({ email, exp: bearerExp }, process.env.JWT_SECRET);
+      const refreshToken = jwt.sign({ email, exp: refreshExp}, process.env.JWT_REFRESH_SECRET)
       res.status(200).json({
-        token,
-        tokenType: "Bearer",
-        expires_in,
+        bearerToken: {
+          token: bearerToken,
+          token_type: "Bearer",
+          expires_in: 600,
+        },
+        refreshToken: {
+          token: refreshToken,
+          token_type: "Refresh",
+          expires_in: 86400,
+        },
       });
+      // Store refresh token into database
+      return queryUsers.update({refresh_token: refreshToken})
+      
+    }).catch((error) => {
+      res.status(500).json({ error: true, message: error.message });
+      return;
     });
-});
+  });
 // 2.1.1 If passwords match, return JWT
 
 // 2.2 If user does not exist, return error response
